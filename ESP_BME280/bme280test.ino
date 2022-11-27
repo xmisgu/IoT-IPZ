d90dc097550d14e814d6be84a0d7be093b156047
Aleksander Wojciechowski <olekwu1@gmail.com>
2022-11-27 22:27:35 +0100

    ESP reading data from sensor

---
+++ ESP_BME280/bme280test.ino
@@ -0,0 +1,51 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME280 bme; // I2C
+
+unsigned long delayTime;
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println(F("BME280 test"));
+
+    unsigned status;
+    
+    // default settings
+    status = bme.begin(0x76);  
+
+    if (!status) {
+        while (1){
+          Serial.println("Cannot connect");
+          delay(1000);
+        } 
+    }
+    
+}
+
+void loop() { 
+    Serial.print("Temperature = ");
+    Serial.print(bme.readTemperature());
+    Serial.println(" °C");
+
+    Serial.print("Pressure = ");
+
+    Serial.print(bme.readPressure() / 100.0F);
+    Serial.println(" hPa");
+
+    Serial.print("Approx. Altitude = ");
+    Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+    Serial.println(" m");
+
+    Serial.print("Humidity = ");
+    Serial.print(bme.readHumidity());
+    Serial.println(" %");
+
+    Serial.println();
+    delay(2000);
+}
+
